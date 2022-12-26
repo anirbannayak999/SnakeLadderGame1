f188a755f8f4ca297e8238e926c56a9e483a75c6
“anirban <anirbannayak999@gmail.com>
2022-12-26 15:22:04 +0530

    Till player reach 100

--- src/com/bridgelabz/snakeladdergame.java
+++ src/com/bridgelabz/snakeladdergame.java
@@ -1,11 +1,13 @@
 package com.bridgelabz;
 
-public class snakeladdergame {
-    static final int START_POSITION = 0;
+public class snakeladdergame {static final int START_POSITION = 0;
+    static final int FINAL_POSITION = 100;
 
     static final int NO_PLAY = 0;
     static final int IS_SNAKE = 1;
     static final int IS_LADDER = 2;
+
+    static int diceCount =0;
 
     static int diceRoll() {
         int x = (int) (1+ Math.random() * 6);
@@ -21,28 +23,44 @@ public class snakeladdergame {
 
         int playerPosition = START_POSITION;
 
-        int roll = diceRoll();
-        System.out.println(roll);
+        while (playerPosition <= FINAL_POSITION) {
+            diceCount++;
 
-        int option = getOption();
-        switch (option) {
-            case IS_SNAKE :
-                System.out.println("Snake");
-                playerPosition -= roll;
-                break;
-            case IS_LADDER :
-                System.out.println("Ladder");
-                playerPosition += roll;
-                break;
-            default:
-                System.out.println("No play");
+            int roll = diceRoll();
+            System.out.println(roll);
+
+            int option = getOption();
+            switch (option) {
+                case IS_SNAKE :
+                    System.out.println("Snake");
+                    playerPosition -= roll;
+                    if(playerPosition < START_POSITION)
+                        playerPosition = START_POSITION;
+                    System.out.println("Player position : " + playerPosition);
+                    break;
+                case IS_LADDER :
+                    System.out.println("Ladder");
+                    playerPosition += roll;
+                    System.out.println("Player position : " + playerPosition);
+                    break;
+                default:
+                    System.out.println("No play");
+                    System.out.println("Player position : " + playerPosition);
+
+            }
 
         }
 
-        System.out.println("Player position : " + playerPosition);
+
+        System.out.print("Total Dice count : " + diceCount);
+        System.out.println("Finally Player position : " + playerPosition);
 
     }
 
 }
+
+
+
+
 
 
