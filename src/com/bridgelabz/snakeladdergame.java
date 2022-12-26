a4f695240f622827b4b4e9d24be41bbf8b3f3643
“anirban <anirbannayak999@gmail.com>
2022-12-26 13:19:11 +0530

    starting position player 1

---
+++ src/com/bridgelabz/snakeladdergame.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class snakeladdergame {
+    static final int START_POSITION = 0;
+    static final int FINAL_POSITION = 100;
+
+
+    public static void main(String[] args) {
+        System.out.println("welcome to snake ladder game");
+
+
+
+    }
+}
